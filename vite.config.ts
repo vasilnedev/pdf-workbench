ae111b5ce673300d3002ca0793125a7fd09439be
vasilnedev <vasilnedev55@gmail.com>
2025-04-14 20:48:43 +0100

    Ubstall Chakra

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     allowedHosts: ['localhost','bim-app.laptop'], // allow access from localhost and the specified hostname
     port: 5501
